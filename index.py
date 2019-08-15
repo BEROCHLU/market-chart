6d02e3122daf23c8a4756af54027bef9dd45a559
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2019-08-15 15:26:03 +0900

    toriaezu

--- index.py
+++ index.py
@@ -4,32 +4,19 @@ from bottle import get, request, response, route, run, template
 @get('/')
 def index():
     try:
-        ticker = request.query.q
-        print(ticker)
+        qq = request.query.q
+        print(qq)
+
+        yfq = yf.Ticker(qq)
+
+        dfHist = yfq.history(period="6mo")
+        dfHist = dfHist.drop(columns=['Volume', 'Dividends', 'Stock Splits'])
+        dfHist.to_csv(f'{qq}.csv')
+
         return template('index')
+
     except:
         return template('index')
-    #q = request.query.get('q')
-    #q = urllib.parse.urlparse('q')
-    #qq = request.args.get('name')
-
-    #print(qq)
-    
-    #return template('index')
-
-@route('/<name>')
-def geticker(name):
-    
-    spy = yf.Ticker(name)
-
-    dfHist = spy.history(period="6mo")
-    dfHist = dfHist.drop(columns=['Volume', 'Dividends', 'Stock Splits'])
-
-    dfHist.to_csv(f'{name}.csv')
-
-    print(name)
-
-    return template('index')
 
 if __name__=='__main__':
     run(host='localhost', port=80)
