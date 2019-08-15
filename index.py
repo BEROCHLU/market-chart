8826a494126f0f25768d9569f3d11826a18f06e2
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2019-08-15 16:37:21 +0900

    toriaezu

--- index.py
+++ index.py
@@ -6,17 +6,19 @@ def index():
     try:
         qq = request.query.q
         print(qq)
+        if qq:
+            yfq = yf.Ticker(qq)
 
-        yfq = yf.Ticker(qq)
+            dfHist = yfq.history(period="6mo")
+            dfHist = dfHist.drop(columns=['Volume', 'Dividends', 'Stock Splits'])
+            dfHist.to_csv(f'{qq}.csv')
+        else:
+            return template('index', qq=None)
 
-        dfHist = yfq.history(period="6mo")
-        dfHist = dfHist.drop(columns=['Volume', 'Dividends', 'Stock Splits'])
-        dfHist.to_csv(f'{qq}.csv')
-
-        return template('index')
+        return template('index', qq=qq)
 
     except:
-        return template('index')
+        return template('index', qq='error')
 
 if __name__=='__main__':
     run(host='localhost', port=80)
