d471c36655a1d0e339bb8398ac5c7d261f7d94a3
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2019-11-02 18:12:59 +0900

    80 to 8080

--- index.py
+++ index.py
@@ -29,4 +29,4 @@ if __name__=='__main__':
     if os.path.exists('./.ifdef'):
         run(host='localhost', port=80, debug=True)
     else:
-        run(host='0.0.0.0', port=80, reloader=True)
+        run(host='0.0.0.0', port=8080, reloader=True)
