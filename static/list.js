f585d89ebc740c7ce0c48c671d36ef0dd3b135f5
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2020-06-24 22:53:24 +0900

    .

--- static/list.js
+++ static/list.js
@@ -2,5 +2,5 @@ export const arrTicker = [
     'SPY', 'DIA', 'QQQ', 'IWM', 'FDN', 'VYM', 'GS', 'MS', 'JPM', 'WFC', 'C', 'BAC', 'BCS', 'DB', 'FB', 'AAPL', 'NFLX', 'GOOG', 'AMZN', 'MSFT',
     'TWTR', 'SNAP', 'SQ', 'AMD', 'NVDA', 'BTC-USD', 'SPXL', 'UPRO', 'UDOW', 'TECL', 'TQQQ', 'TNA', 'SPXS', 'SPXU', 'SDOW', 'TECS', 'SQQQ', 'TZA', 'FAZ', 'VXX', 'UVXY', 'TVIX',
     'GLD', 'USO', 'TLT', 'BA', 'UNH', 'MMM', 'HD', 'MCD', 'V', 'JNJ', 'GE', 'BRK-B', 'CVX', 'PG', 'WMT', 'XOM', 'FSLY', 'NET', 'DDOG', 'DT', 'ROKU', 'NIO',
-    'MRNA', 'CODX', 'VIR', 'INO', 'SRNE', 'MDB', 'WORK', 'OKTA', 'SHOP', 'CURE', 'CCOI', 'CHGG', 'SAM', 'CNX', 'FFIN', 'TDOC', 'TREX', 'FIVN', 'BCPC', 'FR', 'POR'
+    'MRNA', 'CODX', 'VIR', 'INO', 'SRNE', 'MDB', 'WORK', 'OKTA', 'SHOP', 'CURE', 'CCOI', 'CHGG', 'SAM', 'CNX', 'FFIN', 'TDOC', 'TREX', 'FIVN', 'BCPC', 'FR', 'POR', '^FTSE'
 ];
