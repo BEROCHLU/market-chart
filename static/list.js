7f0de54d793a795520288eb8bca8f5254100f2bf
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2020-07-11 00:51:17 +0900

    .

--- static/list.js
+++ static/list.js
@@ -2,6 +2,6 @@ export const arrTicker = [
     'SPY', 'DIA', 'QQQ', 'IWM', 'FDN', 'VYM', 'GS', 'MS', 'JPM', 'WFC', 'C', 'BAC', 'BCS', 'DB', 'FB', 'AAPL', 'NFLX', 'GOOG', 'AMZN', 'MSFT',
     'TWTR', 'SQ', 'AMD', 'NVDA', 'BTC-USD', 'SPXL', 'UPRO', 'UDOW', 'TECL', 'TQQQ', 'TNA', 'SPXS', 'SPXU', 'SDOW', 'TECS', 'SQQQ', 'TZA', 'FAZ', 'VXX', 'UVXY', 'TVIX',
     'GLD', 'USO', 'TLT', 'BA', 'UNH', 'MMM', 'HD', 'MCD', 'V', 'JNJ', 'GE', 'BRK-B', 'CVX', 'PG', 'WMT', 'XOM', 'FSLY', 'NET', 'DDOG', 'DT', 'ROKU', 'NIO',
-    'MRNA', 'CODX', 'VIR', 'INO', 'SRNE', 'MDB', 'WORK', 'OKTA', 'SHOP', 'CURE', 'CCOI', 'DECK', 'BJ', 'OVV', 'FFIN', 'LHCG', 'SITE', 'BL', 'BCPC', 'EGP', 'OGS',
-    '^FTSE', '^RUT'
+    'MRNA', 'CODX', 'VIR', 'INO', 'SRNE', 'MDB', 'WORK', 'OKTA', 'SHOP', 'CURE', 'XLK', 'FXI',
+    '^FTSE', 'JPY=X', 'GBPJPY=X'
 ];
