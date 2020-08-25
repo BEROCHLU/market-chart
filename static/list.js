cff05f10b3826b2a3c407e8bc6e0d92ac908cd96
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2020-08-25 20:24:18 +0900

    add pyanywhere

--- static/list.js
+++ static/list.js
@@ -1,7 +1,7 @@
 export const arrTicker = [
     'SPY', 'DIA', 'QQQ', 'IWM', 'FDN', 'VYM', 'GS', 'MS', 'JPM', 'WFC', 'C', 'BAC', 'BCS', 'DB', 'FB', 'AAPL', 'NFLX', 'GOOG', 'AMZN', 'MSFT',
-    'TWTR', 'SQ', 'AMD', 'NVDA', 'BTC-USD', 'SPXL', 'UPRO', 'UDOW', 'TECL', 'TQQQ', 'TNA', 'SPXS', 'SPXU', 'SDOW', 'TECS', 'SQQQ', 'TZA', 'FAZ', 'VXX', 'UVXY', 'TVIX',
+    'TWTR', 'SQ', 'AMD', 'NVDA', 'BTC-USD', 'SPXL', 'UPRO', 'UDOW', 'TECL', 'TQQQ', 'TNA', 'SPXS', 'SPXU', 'SDOW', 'TECS', 'SQQQ', 'TZA', 'FAZ', 'VXX', 'UVXY',
     'GLD', 'USO', 'TLT', 'BA', 'UNH', 'MMM', 'HD', 'MCD', 'V', 'JNJ', 'GE', 'BRK-B', 'CVX', 'PG', 'WMT', 'XOM', 'FSLY', 'NET', 'DDOG', 'DT', 'ROKU', 'NIO',
-    'MRNA', 'CODX', 'VIR', 'INO', 'SRNE', 'MDB', 'WORK', 'OKTA', 'SHOP', 'CURE', 'XLK', 'FXI',
+    'MDB', 'WORK', 'OKTA', 'SHOP', 'CURE', 'XLK', 'FXI', 'MRNA', 'CODX', 'VIR', 'INO', 'SRNE',  'BNTX', 'VXRT', 'NVAX', 'BCRX',
     '^FTSE', 'JPY=X', 'GBPJPY=X'
 ];
