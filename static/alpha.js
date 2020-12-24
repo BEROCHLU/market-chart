b8117266bc3c38ec8509f2fdb154446a05ebcbea
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2020-12-24 23:58:14 +0900

    .

--- static/alpha.js
+++ static/alpha.js
@@ -39,8 +39,8 @@ document.querySelector('#alpha').addEventListener('click', () => {
             plot_min = _.floor(plot_min * 0.97);
             plot_max = _.ceil(plot_max * 1.03);
 
-            const strGridL = '5%';
-            const strGridR = '8%';
+            const strGridL = '10%';
+            const strGridR = '2%';
 
             let option = {
                 title: {
--- static/list.js
+++ static/list.js
@@ -2,6 +2,6 @@ export const arrTicker = [
     'SPY', 'DIA', 'QQQ', 'IWM', 'FDN', 'VYM', 'GS', 'MS', 'JPM', 'WFC', 'C', 'BAC', 'BCS', 'DB', 'FB', 'AAPL', 'NFLX', 'GOOG', 'AMZN', 'MSFT',
     'TWTR', 'SQ', 'AMD', 'NVDA', 'BTC-USD', 'SPXL', 'UPRO', 'UDOW', 'TECL', 'TQQQ', 'TNA', 'SPXS', 'SPXU', 'SDOW', 'TECS', 'SQQQ', 'TZA', 'FAZ', 'VXX', 'UVXY',
     'GLD', 'USO', 'TLT', 'BA', 'UNH', 'MMM', 'HD', 'MCD', 'V', 'JNJ', 'GE', 'BRK-B', 'CVX', 'PG', 'WMT', 'XOM', 'FSLY', 'NET', 'DDOG', 'DT', 'ROKU', 'NIO',
-    'MDB', 'WORK', 'OKTA', 'SHOP', 'CURE', 'XLK', 'FXI', 'MRNA', 'CODX', 'VIR', 'INO', 'SRNE',  'BNTX', 'VXRT', 'NVAX', 'BCRX', 'CHGG',
-    '^FTSE', '^FCHI', '^GDAXI', '^RUT', 'JPY=X', 'GBPJPY=X', 'CRSR', 'PLTR', 'CVAC', 'SUMO', 'WEBL', 'WEBS'
+    'MDB', 'OKTA', 'SHOP', 'CURE', 'XLK', 'FXI', 'MRNA', 'CODX', 'VIR', 'INO', 'SRNE',  'BNTX', 'VXRT', 'NVAX', 'BCRX', 'CHGG',
+    '^FTSE', '^FCHI', '^GDAXI', '^RUT', 'JPY=X', 'GBPJPY=X', 'CRSR', 'PLTR', 'CVAC', 'SUMO', 'WEBL', 'WEBS', 'U', 'AI', 'ABNB', 'DASH'
 ];
