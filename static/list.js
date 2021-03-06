e6f476b5a691f94eda7fe6560b955ea153b3fb83
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2021-03-06 15:45:04 +0900

    css

--- static/list.js
+++ static/list.js
@@ -1,7 +1,6 @@
 export const arrTicker = [
-    'SPY', 'DIA', 'QQQ', 'IWM', 'FDN', 'VYM', 'GS', 'MS', 'JPM', 'WFC', 'C', 'BAC', 'BCS', 'DB', 'FB', 'AAPL', 'NFLX', 'GOOG', 'AMZN', 'MSFT',
-    'TWTR', 'SQ', 'AMD', 'NVDA', 'BTC-USD', 'SPXL', 'UPRO', 'UDOW', 'TECL', 'TQQQ', 'TNA', 'SPXS', 'SPXU', 'SDOW', 'TECS', 'SQQQ', 'TZA', 'FAZ', 'VXX', 'UVXY',
-    'GLD', 'USO', 'TLT', 'BA', 'UNH', 'MMM', 'HD', 'MCD', 'V', 'JNJ', 'GE', 'BRK-B', 'CVX', 'PG', 'WMT', 'XOM', 'FSLY', 'NET', 'DDOG', 'DT', 'ROKU', 'NIO',
-    'MDB', 'OKTA', 'SHOP', 'CURE', 'XLK', 'FXI', 'MRNA', 'CODX', 'VIR', 'INO', 'SRNE',  'BNTX', 'VXRT', 'NVAX', 'BCRX', 'CHGG',
-    '^FTSE', '^FCHI', '^GDAXI', '^RUT', 'JPY=X', 'GBPJPY=X', 'CRSR', 'PLTR', 'CVAC', 'SUMO', 'WEBL', 'WEBS', 'U', 'AI', 'ABNB', 'DASH'
+    'AAPL', 'ABNB', 'AI', 'AMC', 'AMD', 'AMZN', 'BA', 'BAC', 'BCRX', 'BCS', 'BNTX', 'BRK-B', 'BTC-USD', 'C', 'CODX', 'CRSR', 'CVAC', 'CVX', 'DASH', 'DB', 'DDOG', 'DIA', 'DT', 'FAZ', 'FB', 'FSLY', 'FXI',
+    'GE', 'GLD', 'GME', 'GOOG', 'GS', 'HD', 'INO', 'IWM', 'JNJ', 'JPM', 'MCD', 'MDB', 'MMM', 'MRNA', 'MS', 'MSFT', 'NET', 'NFLX', 'NIO', 'NVAX', 'NVDA', 'OKTA', 'PFE', 'PG', 'PLTR', 'QQQ', 'ROKU',
+    'SDOW', 'SHOP', 'SNOW', 'SPXL', 'SPXS', 'SPXU', 'SPY', 'SQ', 'SQQQ', 'SRNE', 'SUMO', 'TECL', 'TECS', 'TLT', 'TNA', 'TQQQ', 'TWTR', 'TZA', 'U', 'UDOW', 'UNH', 'UPRO', 'USO', 'UVXY',
+    'V', 'VIR', 'VXRT', 'VXX', 'VYM', 'WEBL', 'WEBS', 'WFC', 'WMT', 'XOM', '^FCHI', '^FTSE', '^GDAXI', '^RUT', 'NEE', 'FROG', 'ASAN', 'PLAN', 'FAS', 'SOXL', 'SOXS', 'ARKK'
 ];
