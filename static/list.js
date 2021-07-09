e8f1005a1f84fcc89e83f556bdf0efeb146b4b23
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2021-07-09 23:12:23 +0900

    .

--- static/list.js
+++ static/list.js
@@ -3,5 +3,5 @@ export const arrTicker = [
     'GE', 'GLD', 'GME', 'GOOG', 'GS', 'HD', 'IWM', 'JNJ', 'JPM', 'MCD', 'MDB', 'MMM', 'MRNA', 'MS', 'MSFT', 'NET', 'NFLX', 'NIO', 'NVAX', 'NVDA', 'OKTA', 'PFE', 'PG', 'PLTR', 'QQQ', 'ROKU',
     'SDOW', 'SHOP', 'SNOW', 'SPXL', 'SPXS', 'SPXU', 'SPY', 'SQ', 'SQQQ', 'SUMO', 'TECL', 'TECS', 'TLT', 'TNA', 'TQQQ', 'TWTR', 'TZA', 'U', 'UDOW', 'UNH', 'UPRO', 'CL%3DF', 'UVXY', 'V', 'VXX', 'VYM',
     'WFC', 'WMT', 'XOM', '^FCHI', '^FTSE', '^GDAXI', '^RUT', 'NEE', 'FROG', 'ASAN', 'PLAN', 'FAS', 'SOXL', 'SOXS', 'ARKK', 'TSLA', 'NKLA', 'SKLZ', 'OPEN', 'XRP-USD', 'FEYE', 'TDC', 'NTAP', 'AYX',
-    'RBLX', 'PATH', 'SPCE'
+    'RBLX', 'PATH', 'SPCE', 'PUBM', 'FUBO'
 ];
