362dd97fc03ad9ef99d09dd0850e09026db117fd
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2021-09-22 10:08:03 +0900

    .

--- local.py
+++ local.py
@@ -4,7 +4,6 @@
 import base64
 import datetime
 import json
-import os
 
 import pandas as pd
 import requests
@@ -24,7 +23,6 @@ f1 = lambda ms: datetime.datetime.fromtimestamp(ms, tz=edt).strftime("%Y-%m-%d")
 # hash
 str_ua = b"TW96aWxsYS81LjAgKE1hY2ludG9zaDsgSW50ZWwgTWFjIE9TIFggMTFfNikgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzkzLjAuNDU3Ny44MiBTYWZhcmkvNTM3LjM2"
 # 起動したディレクトリがHOMEになるので./publicを追加する
-HOME_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_PATH.append("./public")
 
 
