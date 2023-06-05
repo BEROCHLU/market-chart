1ad2323a5218302eb6bdd034128c7f600b6ca22d
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2023-06-05 15:23:48 +0900

    secure update

--- public/middle.php
+++ public/middle.php
@@ -1,8 +1,16 @@
 <?php
-    $t = $_GET["t"];
-    $r = $_GET["r"];
-    $i = $_GET["i"];
-    exec("python3 /virtual/pleasecov/public_html/pipm/wakeup.py $t $r $i", $arrOut);
+    // Input validation is needed here
+    $t = escapeshellarg($_GET["t"]);
+    $r = escapeshellarg($_GET["r"]);
+    $i = escapeshellarg($_GET["i"]);
+
+    $command = "python3 /virtual/pleasecov/public_html/pipm/wakeup.py $t $r $i";
+    $result = exec($command, $arrOut, $returnVar);
+
     header("Content-Type: application/json; charset=utf-8");
-    echo $arrOut[0];
+    if($returnVar !== 0) {
+        echo json_encode(['error' => 'Script execution failed']);
+    } else {
+        echo $arrOut[0];
+    }
 ?>
